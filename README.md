82cee0cb8222edbbd4af7b65e47bb140fe0738c9
dev <user@users.noreply.github.com>
2021-06-05 17:05:09 +0300

    5

--- README.md
+++
@@ -1,1 +0,0 @@
-# lol-documentacion.github.io
---
+++ es/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Argumentos de la línea de comandos de TeamRedMiner [2021]",
+        "excerpt":"You can use this list as a reference for all Launch Parameters supported by TeamRedMiner.    You can download TeamRedMiner 1.29 from here:   Download Now   Download (mirror)   Linux:  Under Linux you need to replace TeamRedMiner.exe with ./TeamRedMiner in the command-line examples below.   ","categories": ["documentation"],
+        "tags": ["TeamRedMiner"],
+        "url": "https://lolminer.site/documentation/setup/",
+        "teaser": "https://lolminer.site/assets/images/thumbs/500x300.png"
+      },{
+        "title": "Download lolMiner  1.29 - AMD GPU Miner [2021]",
+        "excerpt":" Get started with lolMiner  \t\t \t\t\t \t\tVersion: 1.29  \t\tFile: *lolminer.exe,    \t\tSHA256: 2736e4a8db08f28d126d069febbc41af4f6b46c4da80ac6ab407e474aad48c8a  \t\t(Download for Linux)\t \t\t \t\t \t\t \t\t \t\tDownload Now (Windows x64) \t\t \t\t(mirror) \t\t \t     The new lolMiner stable version is ready.   You can download lolMiner 1.29 from here:   Download Now   Download (mirror)   Inside the archive you will find a file README.txt with installation instructions.   Make sure to replace the pool and wallet address by what you’re using in all files.        ","categories": ["download"],
+        "tags": ["lolMiner"],
+        "url": "https://lolminer.site/download/1.29/",
+        "teaser": "https://lolminer.site/assets/images/thumbs/500x300.png"
+      },]
