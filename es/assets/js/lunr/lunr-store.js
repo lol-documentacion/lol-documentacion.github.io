e6649f42eb122fb46fdb2c675d75f33176423c5e
dev <user@users.noreply.github.com>
2022-03-18 13:37:44 +0300

    zill 2

--- es/assets/js/lunr/lunr-store.js
+++ es/assets/js/lunr/lunr-store.js
@@ -1,12 +1,12 @@
 var store = [{
-        "title": "Argumentos de la línea de comandos de LolMiner [2021]",
-        "excerpt":"You can use this list as a reference for all Launch Parameters supported by LolMiner.    You can download LolMiner 1.46a from here:   Download Now   Download (mirror)   Linux:  Under Linux you need to replace LolMiner.exe with ./LolMiner in the command-line examples below.   ","categories": ["documentation"],
+        "title": "Argumentos de la línea de comandos de LolMiner [2022]",
+        "excerpt":"You can use this list as a reference for all Launch Parameters supported by LolMiner.          You can download LolMiner 1.46a from here:   Download Now   Download (mirror)   Linux:  Under Linux you need to replace LolMiner.exe with ./LolMiner in the command-line examples below.   ","categories": ["documentation"],
         "tags": ["LolMiner"],
         "url": "https://lolminer.site/documentation/setup/",
         "teaser": "https://lolminer.site/assets/images/thumbs/500x300.png"
       },{
-        "title": "Download lolMiner  1.29 - AMD GPU Miner [2021]",
-        "excerpt":" Get started with lolMiner  \t\t \t\t\t \t\tVersion: 1.29  \t\tFile: *lolminer.exe,    \t\tSHA256: 2736e4a8db08f28d126d069febbc41af4f6b46c4da80ac6ab407e474aad48c8a  \t\t(Download for Linux)\t \t\t \t\t \t\t \t\t \t\tDownload Now (Windows x64) \t\t \t\t(mirror) \t\t \t     The new lolMiner stable version is ready.   You can download lolMiner 1.29 from here:   Download Now   Download (mirror)   Inside the archive you will find a file README.txt with installation instructions.   Make sure to replace the pool and wallet address by what you’re using in all files.        ","categories": ["download"],
+        "title": "Download lolMiner  1.29 - AMD GPU Miner [2022]",
+        "excerpt":" Get started with lolMiner  \t\t \t\t\t \t\tVersion: 1.29  \t\tFile: *lolminer.exe,    \t\tSHA256: 2736e4a8db08f28d126d069febbc41af4f6b46c4da80ac6ab407e474aad48c8a  \t\t(Download for Linux)\t \t\t \t\t \t\t \t\t \t\tDownload Now (Windows x64) \t\t \t\t(mirror) \t\t \t           The new lolMiner stable version is ready.   You can download lolMiner 1.29 from here:   Download Now   Download (mirror)   Inside the archive you will find a file README.txt with installation instructions.   Make sure to replace the pool and wallet address by what you’re using in all files.        ","categories": ["download"],
         "tags": ["lolMiner"],
         "url": "https://lolminer.site/download/1.29/",
         "teaser": "https://lolminer.site/assets/images/thumbs/500x300.png"
